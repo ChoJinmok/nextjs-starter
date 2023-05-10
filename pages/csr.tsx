f27fb8bd051c466867ce66c3ff2ad775d0d75aa1
ChoJinmok <jjmok1127@gmail.com>
2023-05-10 21:59:53 +0900

    feat: CSR -> 리액트와 동일

--- pages/csr.tsx
+++ pages/csr.tsx
@@ -1,25 +1,19 @@
 import Head from 'next/head';
-import Link from 'next/link';
 
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/Home.module.css';
 
-type Time = string;
+export default function Home() {
+  const [time, setTime] = useState('');
 
-// getServerSideProps는 props를 return해야하는 규칙이 있음
-// return한 props는 해당 페이지에 전달된다.
-// 데이터를 서버에서 fetching할 수도 있음
-export const getServerSideProps:GetServerSideProps<{ time: Time }> = async () => {
-  // 서버에서 실행
-  console.log('server');
+  useEffect(() => {
+    // 브라우저에서 실행
+    console.log('client');
 
-  return {
-    props: { time: new Date().toISOString() },
-  };
-};
+    setTime(new Date().toISOString());
+  }, []);
 
-export default function Home({ time }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={styles.container}>
       <Head>
@@ -30,11 +24,6 @@ export default function Home({ time }: InferGetServerSidePropsType<typeof getSer
       <main>
         <h1 className={styles.title}>
           {time}
-        </h1>
-        <h1>
-          <Link href="/csr">
-            CSR 로
-          </Link>
         </h1>
       </main>
 
